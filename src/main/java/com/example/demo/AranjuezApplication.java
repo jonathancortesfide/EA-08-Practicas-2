b63206bc1a64159417850c7eb8628a51a6d1e3c9
jonathancortesfide <jcortes20382@ufide.ac.cr>
2024-10-08 23:50:15 -0600

    initial commit

---
+++ src/main/java/com/example/demo/AranjuezApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AranjuezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AranjuezApplication.class, args);
+	}
+
+}
